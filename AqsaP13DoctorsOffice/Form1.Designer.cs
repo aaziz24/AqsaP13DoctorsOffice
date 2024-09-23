5b63204ff8074ce7e3f048fe96343af6e171d7c2
aaziz24 <aqsa.aziz07@student.qcc.cuny.edu>
2024-09-23 12:13:08 -0400

    ICA 1

--- AqsaP13DoctorsOffice/Form1.Designer.cs
+++ AqsaP13DoctorsOffice/Form1.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            btnCalculate = new Button();
+            btnClear = new Button();
+            btnQuit = new Button();
+            lstOut = new ListBox();
             SuspendLayout();
             // 
             // label1
@@ -41,11 +49,89 @@
             label1.Text = "Aqsa's Medical Clinic";
             label1.Click += label1_Click;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(92, 121);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Patient Name";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(92, 164);
+            label3.Name = "label3";
+            label3.Size = new Size(85, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Insurance Type";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(255, 118);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(159, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(255, 161);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(159, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(75, 359);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(117, 68);
+            btnCalculate.TabIndex = 5;
+            btnCalculate.Text = "Calculate &Insurance Rate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(355, 359);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 68);
+            btnClear.TabIndex = 6;
+            btnClear.Text = "&Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += button2_Click;
+            // 
+            // btnQuit
+            // 
+            btnQuit.Location = new Point(600, 359);
+            btnQuit.Name = "btnQuit";
+            btnQuit.Size = new Size(75, 68);
+            btnQuit.TabIndex = 7;
+            btnQuit.Text = "&Quit";
+            btnQuit.UseVisualStyleBackColor = true;
+            // 
+            // lstOut
+            // 
+            lstOut.FormattingEnabled = true;
+            lstOut.ItemHeight = 15;
+            lstOut.Location = new Point(92, 199);
+            lstOut.Name = "lstOut";
+            lstOut.Size = new Size(583, 139);
+            lstOut.TabIndex = 8;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lstOut);
+            Controls.Add(btnQuit);
+            Controls.Add(btnClear);
+            Controls.Add(btnCalculate);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Aqsa Aziz";
@@ -57,5 +143,13 @@
         #endregion
 
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button btnCalculate;
+        private Button btnClear;
+        private Button btnQuit;
+        private ListBox lstOut;
     }
 }
--- AqsaP13DoctorsOffice/Form1.cs
+++ AqsaP13DoctorsOffice/Form1.cs
@@ -16,5 +16,57 @@ namespace AqsaP13DoctorsOffice
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double taxRate = .0875;
+            double rate, totalRate
+            string PatientName;
+
+            //input
+            price = double.Parse(txtInsuranceRate.Text);
+            PatientName = txtInsuranceRate.Text;
+
+            //processing
+            totalRate = (rate * taxRate) + price;
+            totalPrice = price + taxAmount;
+
+            //output
+            lstOut.Items.Add("Patient Name is " + PatientName);
+            lstOut.Items.Add("Price is " + price.ToString("C2"));
+            lstOut.Items.Add("tax amount is " + taxAmount.ToString("C2"));
+            lstOut.Items.Add("Total Price is " + totalPrice);
+
+            //This changes the focus to the clear button
+            //btnClear.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtPatientName.Clear();
+            txtInsuranceType.Clear();
+            lstOut.Items.Clear();
+            txtPatientName.Focus();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close():
+        }
+
+        private void txtPatientName_Enter(object sender, EventArgs e)
+        {
+            txtPatientName.Backcolor = Color.White Bisque;
+        }
+
+        private void txtPatientName_leave(object sender, EventArgs e)
+        {
+            txtPatientName.Backcolor = SystemColors.Window;
+        }
     }
 }
